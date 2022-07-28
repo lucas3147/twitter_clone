68f80e88afa4fd840fb19f52362d588b92aa69b1
lucas <lucas.souza3146@gmail.com>
2022-07-28 13:15:32 -0300

    Meu Site twitter clone

---
+++ vendor/MF/Model/Container.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MF\Model;
+use App\Connection;
+
+class Container {
+
+    public static function getModel($model){
+
+        $class = "\\App\\Models\\" . ucfirst($model);
+        //retornar o modelo solicitado já instanciado ccom a conexão estabelecida.
+
+        $conn = Connection::getDb();
+
+        return new $class($conn);
+
+    }
+}
+
+?>
